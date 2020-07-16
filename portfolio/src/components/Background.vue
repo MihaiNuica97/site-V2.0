33155da22670315be287ee5694fbf8a3d00bd0d6
MihaiNuica97 <mihainuica@gmail.com>
2020-07-16 16:33:22 +0100

    background fixed

--- portfolio/src/components/Background.vue
+++ portfolio/src/components/Background.vue
@@ -1,19 +1,19 @@
 <template>
-  <div id="bg-outer">
-      <img id="bg-img">
-      <div id="bg-overlay"></div>
-  </div>
+	<div id="bg-outer">
+		<img id="bg-img" />
+		<div id="bg-overlay"></div>
+	</div>
 </template>
 
 <script>
-export default {
-    name: "Background"
-}
+	export default {
+		name: "Background",
+	};
 </script>
 
 <style>
-    #bg-outer{
-        /* z-index: -2; 
+	#bg-outer {
+		/* z-index: -2; 
         width: 100%;
         height: 100%;
         position: fixed;
@@ -21,27 +21,27 @@ export default {
         top: 0;
         background: url('../assets/bg-rain-window.jpg') no-repeat center center;
         background-size: cover; */
-        position: fixed;
-        height: 100%;
-        width: 100%;
-        top: 0;
-        z-index: -2;
-    }
-    #bg-overlay{
-        z-index: -1;
-        width: 100%;
-        height: 100%;
-        background: gray;
-        opacity: 0.1;
-    }
-    #bg-img{
-        height: 100%;
-        width: 100%;
-        content: url('../assets/bg-stripes-purple.jpg');
-    }
-    @media screen and (max-width: 700px){
-        #bg-img{
-            content: url('../assets/bg-stripes-mobile-purple.jpg');
-        }
-    }
+		position: fixed;
+		height: 100%;
+		width: 100%;
+		top: 0;
+		z-index: -2;
+	}
+	/* #bg-overlay {
+		z-index: -1;
+		width: 100%;
+		height: 100%;
+		background: gray;
+		opacity: 0.1;
+	} */
+	#bg-img {
+		height: 100%;
+		width: 100%;
+		content: url("../assets/bg-stripes-purple.jpg");
+	}
+	@media screen and (max-width: 700px) {
+		#bg-img {
+			content: url("../assets/bg-stripes-mobile-purple.jpg");
+		}
+	}
 </style>
