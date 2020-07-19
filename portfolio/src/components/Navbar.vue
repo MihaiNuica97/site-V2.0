95c065d3e5e0786e2d7af250dd260ee6c81a15f6
MihaiNuica97 <mihainuica@gmail.com>
2020-07-19 18:16:03 +0100

    set navbar to fixed

--- portfolio/src/components/Navbar.vue
+++ portfolio/src/components/Navbar.vue
@@ -1,32 +1,29 @@
 <template>
-	<div>
-		<v-app-bar dark class="elevation-0" style="background: transparent">
-			<v-tabs right dark class="mr-0 mr-xl-16" slider-color="primary" >
-				<v-tab v-for="link in links" v-bind:key="link.text" router :to="link.route" >{{link.text}}</v-tab>
-			</v-tabs>
-			<div>
-			</div>
-		</v-app-bar>
-	</div>
-
+  <div class="mb-10">
+    <v-app-bar dark fixed class="elevation-0" style="background: #100824">
+      <v-tabs right dark class slider-color="primary">
+        <v-tab v-for="link in links" v-bind:key="link.text" router :to="link.route">{{link.text}}</v-tab>
+      </v-tabs>
+      <div></div>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
 export default {
-	name:"Navbar",
-	data(){
-		return {
-			links: [
-				{text: "Home", route:'/'},
-				{text: "Projects", route:'/projects'},
-				{text: "Mockups", route:'/mockups'},
-				{text: "Contact", route:'/contact'},
-			]
-		}
-	}
-}
+  name: "Navbar",
+  data() {
+    return {
+      links: [
+        { text: "Home", route: "/" },
+        { text: "Projects", route: "/projects" },
+        { text: "Mockups", route: "/mockups" },
+        { text: "Contact", route: "/contact" }
+      ]
+    };
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
