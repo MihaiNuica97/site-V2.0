9eacb4affa69ceeb1895149599f08ff67866eb90
MihaiNuica97 <mihainuica@gmail.com>
2020-07-21 17:08:33 +0100

    can now pass props through router

--- portfolio/src/router/index.ts
+++ portfolio/src/router/index.ts
@@ -4,48 +4,61 @@ import Home from "../views/Home.vue";
 import Projects from "../views/Projects.vue";
 import Mockups from "../views/Mockups.vue";
 import Contact from "../views/Contact.vue";
+import ExtendedProjects from "../views/ExtendedProjects.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/projects",
-    name: "Projects",
-    component: Projects
-  },
-  {
-    path: "/mockups",
-    name: "Mockups",
-    component: Mockups
-  },
-  {
-    path: "/contact",
-    name: "Contact",
-    component: Contact
-  },
-  {
-    path:"/github",
-    beforeEnter() {location.href = 'https://github.com/MihaiNuica97'}
-  },
-  {
-    path:"/linkedin",
-    beforeEnter() {location.href = 'http://linkedin.com'}
-  },
-  {
-    path:"/instagram",
-    beforeEnter() {location.href = 'https://www.instagram.com/mihai.nuica/'}
-  },
+	{
+		path: "/",
+		name: "Home",
+		component: Home,
+	},
+	{
+		path: "/projects",
+		name: "Projects",
+		component: Projects,
+	},
+	{
+		path: "/projects/:name",
+		name: "ExtendedProjects",
+		component: ExtendedProjects,
+		props: true,
+	},
+	{
+		path: "/mockups",
+		name: "Mockups",
+		component: Mockups,
+	},
+	{
+		path: "/contact",
+		name: "Contact",
+		component: Contact,
+	},
+	{
+		path: "/github",
+		beforeEnter() {
+			location.href = "https://github.com/MihaiNuica97";
+		},
+	},
+	{
+		path: "/linkedin",
+		beforeEnter() {
+			location.href = "http://linkedin.com";
+		},
+	},
+	{
+		path: "/instagram",
+		beforeEnter() {
+			location.href = "https://www.instagram.com/mihai.nuica/";
+		},
+	},
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+	mode: "history",
+	base: process.env.BASE_URL,
+	routes,
 });
 
 export default router;
---
+++ portfolio/src/views/ExtendedProjects.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1 class="white--text">{{name}}</h1>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String
+  },
+  mounted() {
+    console.log(this.name);
+    this.axios
+      .post("http://www.mihainuica.com/getProjects.php", {
+        name: this.name
+      })
+      .then(response => {
+        console.log(response.data);
+      });
+  }
+};
+</script>
+
+<style>
+</style>
